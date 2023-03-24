5c5f4b4d6e59793eacfad464550dd3f37c3befa2
Emma <e.e.gill@outlook.com>
2023-03-24 16:02:32 +0000

    Added scoring system.

--- runner-game.py
+++ runner-game.py
@@ -4,6 +4,11 @@ from sys import exit
 
 
 # HELPER FUNCTIONS
+def display_score():
+    score = int((pygame.time.get_ticks() - start_time) / 1000)
+    score_surf = font.render(f'{score}', False, (100, 149, 237))
+    score_rect = score_surf.get_rect(midtop = (400, 50))
+    screen.blit(score_surf, score_rect)
 
 
 # LET'S GOOOOOO
@@ -13,10 +18,25 @@ pygame.display.set_caption('Endless Runner')
 clock = pygame.time.Clock()
 font = pygame.font.Font('fonts/Pixeltype.ttf', 50)
 
-# Create surfaces
-sky_surface = pygame.image.load('assets/sky.png')
-ground_surface = pygame.image.load('assets/ground.png')
-text_surface = font.render('TEST', False, (255, 255, 255))
+# Create variables
+# Speed variables for game difficulty?
+game_active = True
+start_time = 0
+player_gravity = 0
+
+
+
+# Create surfaces & rectangles
+sky_surf = pygame.image.load('assets/sky.png').convert()
+ground_surf = pygame.image.load('assets/ground.png').convert()
+
+
+
+snail_surf = pygame.image.load('assets/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (750, 300))
+
+player_surf = pygame.image.load('assets/player/player_walk1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80, 300))
 
 while True:
 
@@ -26,9 +46,48 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(sky_surface, (0,0))
-    screen.blit(ground_surface, (0,300))
-    screen.blit(text_surface, (400, 50))
+        if game_active:    
+            if event.type == pygame.KEYDOWN and player_rect.bottom >= 300:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 300:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
+                game_active = True
+
+
+    # Active Gameplay
+    if game_active:
+        # Background and score
+        screen.blit(sky_surf, (0,0))
+        screen.blit(ground_surf, (0,300))
+
+        display_score()
+        #screen.blit(score_surf, score_rect)
+
+        # Snail
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: snail_rect.left = 800
+        screen.blit(snail_surf, snail_rect)
+
+        # Player
+        player_gravity +=1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
+
+        # Collisions
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    
+    else:
+        screen.fill('Blue')
+
 
     # Update the display surface, and set the maximum frame rate (i.e. how often to run the main 'While True' loop)
     pygame.display.update()
